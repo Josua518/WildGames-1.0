19d73d882665db4b1fb9d6e0dc2de0dc0a3a2fe1
Josua518 <84269701+Josua518@users.noreply.github.com>
2021-11-07 16:09:37 +0700

    Add files via upload

---
+++ shark/index.js
@@ -0,0 +1,8 @@
+//-----libs-begin-----
+loadLib("libs/laya.core.js")
+loadLib("libs/laya.ani.js")
+loadLib("libs/laya.ui.js")
+//-----libs-end-------
+window.screenOrientation="portrait",
+loadLib("js/bundle.js");
+
